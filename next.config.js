a1e4a9836e26d58f2c78db911ad8cace4236bdf3
eduardneague <eduard.neague1337@gmail.com>
2023-08-17 12:33:02 +0300

    ignorebuilderrors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  tpyescript: {
+    ignoreBuildErrors: true,
+  },
     experimental: {
       serverActions: true,
       serverComponentsExternalPackages: ["mongoose"],
